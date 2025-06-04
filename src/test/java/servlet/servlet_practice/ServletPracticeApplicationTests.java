44351ff0505540ea29279b967a58464f874aca82
cherish0-0 <jiyu01160@naver.com>
2025-06-04 19:38:50 +0900

    Initial commit

---
+++ src/test/java/servlet/servlet_practice/ServletPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.servlet_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
