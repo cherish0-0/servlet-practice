853d9c1c5d47e182eaa7e6ce485803a0bd24954f
cherish0-0 <jiyu01160@naver.com>
2025-06-04 23:45:06 +0900

    feat: 엔트리 포인트 파일에 어노테이션 추가 서블릿 컴포넌트 자동으로 스캔 및 등록할 수 있도록 @ServletComponentScan 어노테이션 추가

--- src/main/java/servlet/servlet_practice/ServletPracticeApplication.java
+++ src/main/java/servlet/servlet_practice/ServletPracticeApplication.java
@@ -2,7 +2,9 @@ package servlet.servlet_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // This annotation enables scanning for servlet components like @WebServlet, @WebFilter, etc. automatically.
 @SpringBootApplication
 public class ServletPracticeApplication {
 
